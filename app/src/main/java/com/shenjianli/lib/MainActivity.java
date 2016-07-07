2f26a1135af5716d7a29aa6b656a96788c3222b1
shenjianli <cqtddt@163.com>
2016-07-07 16:51:21 +0800

    commit lib structure

---
+++ app/src/main/java/com/shenjianli/lib/MainActivity.java
@@ -0,0 +1,79 @@
+package com.shenjianli.lib;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.shenjianli.lib.api.ApiStores;
+import com.shenjianli.lib.bean.WeatherJson;
+import com.shenjianli.shenlib.RetrofitCallback;
+import com.shenjianli.shenlib.net.NetClient;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.button)
+    Button button;
+    @Bind(R.id.text)
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onClick() {
+        getWeather();
+    }
+
+    private void getWeather() {
+        ApiStores apiStores = NetClient.retrofit().create(ApiStores.class);
+        Call<WeatherJson> call = apiStores.getWeather("101010100");
+        call.enqueue(new Callback<WeatherJson>() {
+
+            @Override
+            public void onFailure(Call<WeatherJson> arg0, Throwable arg1) {
+                Log.i(this.getClass().getSimpleName(), arg1.getStackTrace()+"");
+            }
+
+            @Override
+            public void onResponse(Call<WeatherJson> arg0,
+                                   Response<WeatherJson> arg1) {
+                Log.i("wxl", "getWeatherinfo=" + arg1.body().getWeatherinfo().getCity());
+                text.setText(arg1.body().getWeatherinfo().getCity());
+            }
+
+        });
+    }
+
+    private void getNewWeather() {
+        ApiStores apiStores = NetClient.retrofit().create(ApiStores.class);
+        Call<WeatherJson> call = apiStores.getWeather("101010100");
+        call.enqueue(new RetrofitCallback<WeatherJson>() {
+
+            @Override
+            public void onSuccess(WeatherJson t) {
+                if(null != t){
+                    Log.i(this.getClass().getSimpleName(),t.getWeatherinfo().getCity());
+                }
+            }
+
+            @Override
+            public void onFail(String errorMsg) {
+                Log.e(this.getClass().getSimpleName(),errorMsg);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/shenjianli/lib/api/ApiStores.java
@@ -0,0 +1,27 @@
+package com.shenjianli.lib.api;
+
+
+import com.shenjianli.lib.bean.ApiInfo;
+import com.shenjianli.lib.bean.WeatherJson;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Call<T> get();必须是这种形式,这是2.0之后的新形式
+ * 如果不需要转换成Json数据,可以用了ResponseBody;
+ * 你也可以使用Call<GsonBean> get();这样的话,需要添加Gson转换器
+ */
+public interface ApiStores {
+
+    @GET("adat/sk/{cityId}.html")
+    Call<WeatherJson> getWeather(@Path("cityId") String cityId);
+
+    @POST("client/shipper/getCarType")
+    Call<ResponseBody> getCarType(@Body ApiInfo apiInfo);
+
+}
