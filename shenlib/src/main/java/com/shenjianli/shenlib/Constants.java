7bcc272718e53b9d8bea567ecb5af4c52179e69a
shenjianli <cqtddt@163.com>
2016-07-15 15:56:49 +0800

    提交异常机制及常量文件

---
+++ shenlib/src/main/java/com/shenjianli/shenlib/Constants.java
@@ -0,0 +1,10 @@
+package com.shenjianli.shenlib;
+
+/**
+ * Created by edianzu on 2016/7/14.
+ */
+public class Constants {
+    public static final int CACHE_SIZE = 1024 * 1024 * 50;  //NetClient中http请求缓存的最大值
+
+    public static String SERVER_BASE_URL = "http://www.weather.com.cn/";
+}
