1c70da2b8ce02fcd8fcb45d420baddaeba9ff56e
shenjianli <cqtddt@163.com>
2016-07-21 15:42:41 +0800

    增加变量公共文件

--- shenlib/src/main/java/com/shenjianli/shenlib/Constants.java
+++ shenlib/src/main/java/com/shenjianli/shenlib/Constants.java
@@ -4,6 +4,16 @@ package com.shenjianli.shenlib;
  * Created by edianzu on 2016/7/14.
  */
 public class Constants {
+    /**
+     * 手机代理IP
+     */
+    public static String PROXY;
+    /**
+     * 代理端口
+     */
+    public static int PORT;
+
+
     public static final int CACHE_SIZE = 1024 * 1024 * 50;  //NetClient中http请求缓存的最大值
 
     public static String SERVER_BASE_URL = "http://www.weather.com.cn/";
