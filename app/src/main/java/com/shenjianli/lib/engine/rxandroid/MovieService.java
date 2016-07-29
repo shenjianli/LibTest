00eda6e5b55f593700dd1322e6692f1c5c72d8ce
shenjianli <cqtddt@163.com>
2016-07-29 22:20:35 +0800

    增加rxandroid文件

---
+++ app/src/main/java/com/shenjianli/lib/engine/rxandroid/MovieService.java
@@ -0,0 +1,16 @@
+package com.shenjianli.lib.engine.rxandroid;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * Created by shenjianli on 16/7/29.
+ */
+public interface MovieService {
+    @GET("top250")
+    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+    @GET("top250")
+    Observable<MovieEntity>  getTopRxAndroidMovie(@Query("start") int start, @Query("count") int count);
+}
