02c1bb065079cac600b19f53ae9876e6ccf1015e
shenjianli <cqtddt@163.com>
2016-07-29 22:20:19 +0800

    增加rxandroid文件

---
+++ app/src/main/java/com/shenjianli/lib/activity/RxAndroidActivity.java
@@ -0,0 +1,100 @@
+package com.shenjianli.lib.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.shenjianli.lib.R;
+import com.shenjianli.lib.engine.rxandroid.MovieEntity;
+import com.shenjianli.lib.engine.rxandroid.MovieService;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+
+/**
+ * Created by shenjianli on 16/7/29.
+ */
+public class RxAndroidActivity extends AppCompatActivity {
+
+    @Bind(R.id.click_me_BN)
+    Button clickMeBN;
+    @Bind(R.id.result_TV)
+    TextView resultTV;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_android_layout);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.click_me_BN)
+    public void onClick() {
+        getMovie();
+    }
+
+    //进行网络请求
+    private void getMovie(){
+        String baseUrl = "https://api.douban.com/v2/movie/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MovieService movieService = retrofit.create(MovieService.class);
+        Call<MovieEntity> call = movieService.getTopMovie(0, 10);
+        call.enqueue(new Callback<MovieEntity>() {
+            @Override
+            public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
+                resultTV.setText(response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<MovieEntity> call, Throwable throwable) {
+                resultTV.setText(throwable.getMessage());
+            }
+        });
+    }
+
+    //进行网络请求
+    private void getMovie1(){
+        String baseUrl = "https://api.douban.com/v2/movie/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MovieService movieService = retrofit.create(MovieService.class);
+        movieService.getTopRxAndroidMovie(0,10)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<MovieEntity>() {
+                    @Override
+                    public void onCompleted() {
+                        Toast.makeText(RxAndroidActivity.this, "Get Top Movie Completed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        resultTV.setText(e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(MovieEntity movieEntity) {
+                        resultTV.setText(movieEntity.toString());
+                    }
+                });
+    }
+}
