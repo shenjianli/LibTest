a34065c04d4b878a262ef9159920ad99f0e4766c
shenjianli <cqtddt@163.com>
2016-08-02 12:33:47 +0800

    修改进行测试json对象转字符串

--- app/src/main/java/com/shenjianli/lib/mock/WeatherMockService.java
+++ app/src/main/java/com/shenjianli/lib/mock/WeatherMockService.java
@@ -1,6 +1,7 @@
 package com.shenjianli.lib.mock;
 
 import com.alibaba.fastjson.JSON;
+import com.google.gson.Gson;
 import com.shenjianli.lib.bean.WeatherInfo;
 import com.shenjianli.lib.bean.WeatherJson;
 import com.shenjianli.shenlib.net.mock.MockService;
@@ -18,6 +19,7 @@ public class WeatherMockService extends MockService{
         WeatherJson weatherJson = new WeatherJson();
         weatherJson.setWeatherinfo(weatherinfo);
         String jsonStr = JSON.toJSONString(weatherJson);
+        String result =  new Gson().toJson(weatherJson);
         LogUtils.i("获得的json字符串为：" + jsonStr);
         return jsonStr;
     }
