8a6a7445ea161238b2bb319fe9b884dec6de8029
shenjianli <cqtddt@163.com>
2016-10-13 22:38:50 +0800

    增加获取屏幕信息的工具类

---
+++ shenlib/src/main/java/com/shenjianli/shenlib/util/ScreenUtils.java
@@ -0,0 +1,29 @@
+package com.shenjianli.shenlib.util;
+
+import android.app.Activity;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by shenjianli on 16/10/13.
+ */
+public class ScreenUtils {
+
+    public static String getScreenInfo(Activity activity){
+
+        StringBuffer result = new StringBuffer();
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        DisplayMetrics.DENSITY_HIGH
+        result.append("本手机的屏幕信息：").append("density=").append(metrics.density)
+                .append(",densityDpi").append(metrics.densityDpi)
+                .append(",heightPixels").append(metrics.heightPixels)
+                .append(",widthPixels").append(metrics.widthPixels)
+                .append(",scaledDensity").append(metrics.scaledDensity)
+                .append(",xdpi").append(metrics.xdpi)
+                .append(",ydpi").append(metrics.ydpi);
+
+        return result.toString();
+    }
+
+}
