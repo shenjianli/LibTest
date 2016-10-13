92862ba90b772ac52bbc9e3ad09f226ab0e03389
shenjianli <cqtddt@163.com>
2016-10-13 23:13:28 +0800

    增加适配方案

--- shenlib/src/main/java/com/shenjianli/shenlib/util/ScreenUtils.java
+++ shenlib/src/main/java/com/shenjianli/shenlib/util/ScreenUtils.java
@@ -15,13 +15,56 @@ public class ScreenUtils {
         activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
         result.append("本手机的屏幕信息：").append("density=").append(metrics.density)
-                .append(",densityDpi").append(metrics.densityDpi)
-                .append(",heightPixels").append(metrics.heightPixels)
-                .append(",widthPixels").append(metrics.widthPixels)
-                .append(",scaledDensity").append(metrics.scaledDensity)
-                .append(",xdpi").append(metrics.xdpi)
-                .append(",ydpi").append(metrics.ydpi);
+                .append(",densityDpi=").append(metrics.densityDpi)
+                .append(",heightPixels=").append(metrics.heightPixels)
+                .append(",widthPixels=").append(metrics.widthPixels)
+                .append(",scaledDensity=").append(metrics.scaledDensity)
+                .append(",xdpi=").append(metrics.xdpi)
+                .append(",ydpi=").append(metrics.ydpi);
 
+        result.append("，适配图片=");
+
+
+        switch (metrics.densityDpi){
+            case DisplayMetrics.DENSITY_HIGH:
+                result.append("high");
+                break;
+            case DisplayMetrics.DENSITY_LOW:
+                result.append("low");
+                break;
+            case DisplayMetrics.DENSITY_MEDIUM:
+                result.append("medium");
+                break;
+            case DisplayMetrics.DENSITY_TV:
+                result.append("tv");
+                break;
+            case DisplayMetrics.DENSITY_XHIGH:
+                result.append("xhigh");
+                break;
+            case DisplayMetrics.DENSITY_XXHIGH:
+                result.append("xxhigh");
+                break;
+            case DisplayMetrics.DENSITY_XXXHIGH:
+                result.append("xxxhigh");
+                break;
+            case DisplayMetrics.DENSITY_280:
+                result.append("h-xh");
+                break;
+            case DisplayMetrics.DENSITY_360:
+                result.append("xh-xxh");
+                break;
+            case DisplayMetrics.DENSITY_400:
+                result.append("xh-xxh");
+                break;
+            case DisplayMetrics.DENSITY_420:
+                result.append("xh-xxh");
+                break;
+            case DisplayMetrics.DENSITY_560:
+                result.append("xxh-xxxh");
+                break;
+            default:
+                result.append("not support");
+        }
         return result.toString();
     }
 
